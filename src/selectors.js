f88c6a6652c2a0c33903f7aa1c09cbb213c4b909
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2023-03-15 21:09:13 -0400

    fix quoted idens with access

--- src/selectors.js
+++ src/selectors.js
@@ -7,7 +7,7 @@ import {
 
 export const OWS = '(?:\\s+|(%%(?:%?[^%])*%%))*';
 export const INT = '(?:\\+|-)?(?:\\\\[bqodxz])?[0-9a-z_]+';
-export const VAR = '(?:\\b[A-Za-z_][A-Za-z0-9_]*\\b|`.*`)';
+export const VAR = '(?:\\b[A-Za-z_][A-Za-z0-9_]*\\b|\'.*\')';
 
 export const ANNO_START = `(?:\\:${ lookaheads(['\\:'], true) }|\\?\\:)`;
 export const ASSN_START = `=${ lookaheads(['=', '>'], true) }`;
