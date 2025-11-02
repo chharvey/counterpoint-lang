726954ef734d19b9ff247f4c4a71efc796dd8204
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2025-11-02 01:30:33 -0400

    feat: improve numeric markup

--- src/patterns/tokens.js
+++ src/patterns/tokens.js
@@ -44,8 +44,8 @@ export const NUMBER = {
 	patterns: [
 		{
 			name: 'constant.numeric.radix.cp',
-			match: `(\\+|-)?(${ [
-				digits('[0-1]',    'b'), // `\\\\b([0-1]_?)*[0-1]`
+			match: `(\\+|-)?(?:${ [
+				digits('[0-1]',    'b'), // `(\\\\b)(?:[0-1]_?)*[0-1]`
 				digits('[0-3]',    'q'),
 				digits('[0-5]',    's'),
 				digits('[0-7]',    'o'),
@@ -53,10 +53,26 @@ export const NUMBER = {
 				digits('[0-9a-f]', 'x'),
 				digits('[0-9a-z]', 'z'),
 			].join('|') })`,
+			captures: {
+				1: {name: 'punctuation.operator.cp'},
+				2: {name: 'punctuation.delimiter.radix.cp'},
+				3: {name: 'punctuation.delimiter.radix.cp'},
+				4: {name: 'punctuation.delimiter.radix.cp'},
+				5: {name: 'punctuation.delimiter.radix.cp'},
+				6: {name: 'punctuation.delimiter.radix.cp'},
+				7: {name: 'punctuation.delimiter.radix.cp'},
+				8: {name: 'punctuation.delimiter.radix.cp'},
+			},
 		},
 		{
 			name: 'constant.numeric.decimal.cp',
-			match: `(\\+|-)?${ digits('[0-9]') }(\\.${ digits('[0-9]') }(e(\\+|-)?${ digits('[0-9]') })?)?`,
+			match: `(\\+|-)?${ digits('[0-9]') }(?:(\\.)${ digits('[0-9]') }(?:(e)(\\+|-)?${ digits('[0-9]') })?)?`,
+			captures: {
+				1: {name: 'punctuation.operator.cp'},
+				2: {name: 'punctuation.separator.decimal.cp'},
+				3: {name: 'punctuation.separator.exponent.cp'},
+				4: {name: 'punctuation.operator.cp'},
+			},
 		},
 	],
 };
@@ -72,10 +88,11 @@ export const STRING = {
 	patterns: [
 		{
 			name: 'constant.character.escape.cp',
-			match: `(\\\\)u\\{(${ digits('[0-9a-f]') })?\\}`,
+			match: `(\\\\u\\{)(${ digits('[0-9a-f]') })?(\\})`,
 			captures: {
-				1: {name: 'punctuation.delimiter.cp'},
+				1: {name: 'punctuation.delimiter.escape.cp'},
 				2: {name: 'constant.numeric.hex.cp'},
+				3: {name: 'punctuation.delimiter.escape.cp'},
 			},
 		},
 		{
