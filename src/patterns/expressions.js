14aaec248d61d101e930518cc9c60dbf2827c524
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2025-11-28 09:54:46 -0500

    feat!: rename operator `isnt` -> `!is`

--- src/patterns/expressions.js
+++ src/patterns/expressions.js
@@ -153,7 +153,7 @@ export const EXPRESSIONNONBLOCK = {
 		},
 		{
 			name: pattern_name('keyword.operator.text'),
-			match: '\\b(isset|nat|int|float|dec|is|isnt|if|then|else)\\b',
+			match: '\\b(isset|nat|int|float|dec|is|if|then|else)\\b|!is\\b',
 		},
 		{include: '#ExpressionFunction'},
 		{include: '#ExpressionClass'},
